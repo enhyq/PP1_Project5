90ce81a2b2a50c4565bfbdcf8b5a414f44111c32
Eunhyeok Kwon <eunhyeoq@gmail.com>
2022-12-04 21:00:48 +0900

    Initial commit

---
+++ src/main/java/com/example/JDBCUtil.java
@@ -0,0 +1,25 @@
+package com.example.common;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class JDBCUtil {
+	public static Connection getConnection(){  
+	    Connection con=null;
+	    try{  
+	        Class.forName("com.mysql.jdbc.Driver");  
+	        con= DriverManager.getConnection("jdbc:mysql://walab.handong.edu/p2_21900050","p2_21900050","ueL9ies8Ei");
+	    }catch(Exception e){
+	    	System.out.println(e);
+	    }  
+	    return con;  
+	}  
+	
+	public static void main(String ars[]) {
+		Connection conn = getConnection();
+		if(conn != null)
+			System.out.println("DB 연결됨!");
+		else
+			System.out.println("DB 연결중 오류 !");
+	}
+}
